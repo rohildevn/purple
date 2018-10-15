62111f009a26012f83266d09a1b33b0e233b2c23
Rohildev <rohildev@nimoplanet.com>
2018-10-15 17:42:11 +0530

    Updated contact form v3

--- wedding/js/script.js
+++ wedding/js/script.js
@@ -17,6 +17,44 @@ $(function() {
   
 });
 
+$(function() {
+  
+  // contact form animations
+  $('#contact1').click(function() {
+    $('#contactForm').fadeToggle();
+  })
+  $(document).mouseup(function (e) {
+    var container = $("#contactForm");
+
+    if (!container.is(e.target) // if the target of the click isn't the container...
+        && container.has(e.target).length === 0) // ... nor a descendant of the container
+    {
+        container.fadeOut();
+    }
+  });
+  
+});
+
+$(function() {
+  
+  // contact form animations
+  $('#contact2').click(function() {
+    $('#contactForm').fadeToggle();
+  })
+  $(document).mouseup(function (e) {
+    var container = $("#contactForm");
+
+    if (!container.is(e.target) // if the target of the click isn't the container...
+        && container.has(e.target).length === 0) // ... nor a descendant of the container
+    {
+        container.fadeOut();
+    }
+  });
+  
+});
+
+
+
 (function($) {
     "use strict";
 
