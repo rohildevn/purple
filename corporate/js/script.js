5cb50beedfaeb44beb73267b9c4722385a5e6ff8
Rohildev <rohildev@nimoplanet.com>
2018-10-15 21:17:58 +0530

    Updated corporate and wedding gallery

--- corporate/js/script.js
+++ corporate/js/script.js
@@ -1,3 +1,22 @@
+
+$(function() {
+  
+  // contact form animations
+  $('#contactpopup').click(function() {
+    $('#contactForm').fadeToggle();
+  })
+  $(document).mouseup(function (e) {
+    var container = $("#contactForm");
+
+    if (!container.is(e.target) // if the target of the click isn't the container...
+        && container.has(e.target).length === 0) // ... nor a descendant of the container
+    {
+        container.fadeOut();
+    }
+  });
+  
+});
+
 (function($) {
     "use strict";
 
